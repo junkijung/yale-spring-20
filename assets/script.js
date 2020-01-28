0f52d27f32ed3f94ec772ee5d4d7900aa05df7b2
junkijung <60115978+junkijung@users.noreply.github.com>
2020-01-28 02:48:19 -0500

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function changeText(onMouse) {
+  var i = document.getElementById(onMouse);
+  i.style.display = "inline";
+}
+
+function backtoNormal(onMouse) {
+  var i = document.getElementById(onMouse);
+  i.style.display = "none";
+}
